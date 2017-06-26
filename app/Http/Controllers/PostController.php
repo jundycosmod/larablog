45c1c7b5fd9ebf99ded0fae4c1017bea0250724f
jcagentzero <jundycosmod@gmail.com>
2017-06-26 10:45:10 +0800

    Added larablog source codes.
    Signed-off-by: jcagentzero <jundycosmod@gmail.com>

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Post;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() {
+        
+        $posts = Post::latest()->get();
+        return view('posts.index', compact('posts'));
+        
+    }
+    
+    public function show(Post $post) {
+
+        return view('posts.show', compact('post'));
+        
+    }
+    
+    public function create() {
+        
+        return view('posts.create');
+        
+    }
+    
+    public function store() {
+        
+        $this->validate(request(), [
+            'title' => 'required|max:10',
+            'body' => 'required'
+        ]);
+        Post::create(request(['title', 'body']));
+        
+        return redirect('/');
+        
+    }
+}
---
+++ app/Post.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class Post extends Model
+{
+    
+}
