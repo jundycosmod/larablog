221a09b56e9ebd932bdf71f0965f41e722777821
jcagentzero <jundycosmod@gmail.com>
2017-06-26 22:04:05 +0800

    Laravel from scratch 2017 @episode 22 * Create a unit test.
    Signed-off-by: jcagentzero <jundycosmod@gmail.com>

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -16,8 +17,24 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $first = factory(\App\Post::class)->create();
+        $second = factory(\App\Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+        
+        $posts = \App\Post::archives();
+        
+        $this->assertEquals([
+            [
+                'year' => $first->created_at->format('Y'),
+                'month' => $first->created_at->format('F'),
+                'published' => 1
+            ],
+            [
+                'year' => $second->created_at->format('Y'),
+                'month' => $second->created_at->format('F'),
+                'published' => 1
+            ]
+        ], $posts);
     }
 }
