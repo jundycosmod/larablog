3f51ac76d639e35aa363ed9ec4659d50305d703e
jcagentzero <jundycosmod@gmail.com>
2017-06-28 20:21:13 +0800

    Laravel from scratch 2017 @episode 30 * Tags and Posts * Pivot Table
    Signed-off-by: jcagentzero <jundycosmod@gmail.com>

--- app/Post.php
+++ app/Post.php
@@ -37,5 +37,9 @@ class Post extends Model {
                         ->get()
                         ->toArray();
     }
+    
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 
 }
