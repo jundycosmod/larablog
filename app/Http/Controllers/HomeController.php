52d6fc2d0761e9346622eb02018259856324274a
jcagentzero <jundycosmod@gmail.com>
2017-07-17 19:43:10 +0800

    * Added Clients
    Signed-off-by: jcagentzero <jundycosmod@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 use App\Service;
 use App\Portfolio;
 use App\Experience;
+use App\Client;
 
 class HomeController extends Controller
 {
     
-    public function index(Service $services, Portfolio $portfolios, Experience $experiences) {
+    public function index(Service $services, Portfolio $portfolios, Experience $experiences,
+                          Client $clients) {
         $services = $services->all();
         $portfolios = $portfolios->all();
         $experiences = $experiences->all();
+        $clients = $clients->all();
         
-        return view('layouts.master', compact('services','portfolios','experiences'));
+        return view('layouts.master', compact('services','portfolios','experiences','clients'));
     }
 }
--- resources/views/layouts/clients.blade.php
+++ resources/views/layouts/clients.blade.php
@@ -2,26 +2,14 @@
 <section id="clients">
     <div class="container">
         <div class="row">
+            @foreach($clients as $client)
             <div class="col-md-3 col-sm-6">
-                <a href="#">
-                    <img class="img-fluid d-block mx-auto" src="portfolio/img/logos/envato.jpg" alt="">
+                <a href="{{ $client->website }}">
+                    <img class="img-fluid d-block mx-auto" src="storage/{{ $client->client_logo }}" alt="">
                 </a>
             </div>
-            <div class="col-md-3 col-sm-6">
-                <a href="#">
-                    <img class="img-fluid d-block mx-auto" src="portfolio/img/logos/designmodo.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <a href="#">
-                    <img class="img-fluid d-block mx-auto" src="portfolio/img/logos/themeforest.jpg" alt="">
-                </a>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <a href="#">
-                    <img class="img-fluid d-block mx-auto" src="portfolio/img/logos/creative-market.jpg" alt="">
-                </a>
-            </div>
+            @endforeach
+            
         </div>
     </div>
 </section>
